959914f65250e2d5bc124d701225b0b91d8336ab
Ben Reynolds-Carr <ben.reynolds-carr@hackney.gov.uk>
2019-11-22 12:08:59 +0000

    Refactoring
    * Extracted creation of Program object to setup step (needed to declare
    program variable outside of the methods
    * Changed old assertion (Assert.IsEqual()) format to constraint method
    (Assert.That()
    * changed if statement to ternary statement
    * I will need to change the ternary statement back to extend the
    program, but I wanted to test my knowledge - it required me to force the
    number to be converted to a string, despite returning a variable object.

--- FizzBuzz.Tests/UnitTest1.cs
+++ FizzBuzz.Tests/UnitTest1.cs
@@ -3,30 +3,33 @@ using cSharpFizzBuzz;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
+        Program program;
+
+        [SetUp] 
+        public void Init()
+        {
+            program = new Program();
+        }
 
         [Test]
         public void FizzBuzz_Returns_Number()
         {
-            Program program = new Program();
             Assert.AreEqual(1, program.FizzBuzz(1));
         }
 
         [Test]
         public void FizzBuzz_Returns_Fizz_For_3()
         {
-            Program program = new Program();
             Assert.AreEqual("Fizz", program.FizzBuzz(3));
-
         }
 
         [Test]
         public void FizzBuzz_Returns_Fizz_For_6()
         {
-            Program program = new Program();
-            Assert.AreEqual("Fizz", program.FizzBuzz(6));
-
+            Assert.That(program.FizzBuzz(6), Is.EqualTo("Fizz"));
         }
     }
 }
